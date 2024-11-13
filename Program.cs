1a75f81f5092598c6f589ab13e14574af4c42ae5
William Matsunaga <williamsilvamatsunaga@outlook.com>
2024-11-13 19:28:08 -0400

    Início do código

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+
+        ValidaCPF pessoacpf = new ValidaCPF();
+
+        Console.WriteLine("Digite o cpf: ");
+        string cpf = Console.ReadLine();
+
+
+        bool valido = pessoacpf.CalcularCPF(cpf);
+        if (valido)
+        {
+            Console.WriteLine("Válido");
+        }
+
+        else
+        {
+            Console.WriteLine("Inválido");
+        }
+    }
+}
---
+++ ValidaCPF.cs
@@ -0,0 +1,50 @@
+﻿public class ValidaCPF
+{
+    public string cpf;
+
+    public bool CalcularCPF (string cpf)
+    {
+        cpf = cpf.Replace(".", "").Replace("-", "");
+
+        if (cpf.Length != 11)
+        {
+            return false;
+        }
+
+        int soma = 0;
+        for(int i = 0; i < 9; i++)
+        {
+            int digito = int.Parse(cpf[i].ToString());
+            soma = soma + digito * (10 - i);
+        }
+
+        int primeiroDijitoVerificador = 11 - (soma % 11);
+
+        if (primeiroDijitoVerificador >= 10)
+        {
+            primeiroDijitoVerificador = 0;
+        }
+
+        if (int.Parse(cpf[9].ToString()) != primeiroDijitoVerificador)
+        {
+            return false;
+        }
+
+        soma = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            int digito = int.Parse(cpf[i].ToString());
+            soma = soma + digito * (11 - i);
+        }
+
+        int segundoDigitoVerificador = 11 - ( soma % 11);
+
+        if (segundoDigitoVerificador >= 10)
+        {
+            segundoDigitoVerificador = 0;
+        }
+
+        return int.Parse(cpf[10].ToString()) == segundoDigitoVerificador;
+    }
+}
